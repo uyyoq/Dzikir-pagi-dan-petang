4309ddcd745ea4b4eeb4f2fdff3728c16aed9fb0
Kucing <dilkq@gmail.com>
2021-08-15 18:24:59 +0700

    add purge in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
   purge: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-      './layout/**/*.{js,ts,jsx,tsx}',
-      './helpers/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
   theme: {
