cd51e7ab500f0d2f2c4a4f01fd7a42e7210b7ae5
qipoy <indrabinridwan@gmail.com>
2021-08-15 18:36:49 +0700

    fix: fixeing purge tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+const postcssPreset = [
+  'postcss-preset-env',
+  {
+    autoprefixer: {
+      flexbox: 'no-2009',
+    },
+    stage: 3,
+    features: {
+      'custom-properties': false,
+    },
   },
-}
+];
+module.exports = {
+  plugins: ['postcss-import', 'tailwindcss', 'postcss-nested', 'autoprefixer', 'postcss-flexbugs-fixes', postcssPreset],
+};
