2ce52a5ac9e8be784c31c5ffcb5cb33e1a09b1c9
Kucing <dilkq@gmail.com>
2021-10-16 15:27:16 +0700

    finishing work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [require("tailwindcss"), require("autoprefixer")],
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontSize: {
